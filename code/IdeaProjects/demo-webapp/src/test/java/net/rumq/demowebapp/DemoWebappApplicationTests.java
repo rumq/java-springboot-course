4d73c51605345e40d8524ac72ffd8df810602891
Vikram Pawar <pawar@rumq.net>
2022-11-05 20:51:48 +0000

    Add a a basic web app - now

---
+++ code/IdeaProjects/demo-webapp/src/test/java/net/rumq/demowebapp/DemoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rumq.demowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
