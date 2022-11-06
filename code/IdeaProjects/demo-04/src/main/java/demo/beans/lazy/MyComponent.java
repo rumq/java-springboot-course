ea8c89dd6c231e5dc9bb2d901ea2a6409481ed27
Vikram Pawar <pawar@rumq.net>
2022-11-06 16:18:23 +0000

    Add lazy instantiation example

---
+++ code/IdeaProjects/demo-04/src/main/java/demo/beans/lazy/MyComponent.java
@@ -0,0 +1,17 @@
+package demo.beans.lazy;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class MyComponent {
+    public MyComponent(){
+        System.out.println("MyComponent bean created");
+
+    }
+    @Override
+    public String toString() {
+        return "Hi from MyComponent";
+    }
+}
