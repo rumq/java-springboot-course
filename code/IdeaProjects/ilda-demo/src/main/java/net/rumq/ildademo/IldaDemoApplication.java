8b51766777fe49c49310bdfbbdd620985ee63998
ilda24 <44446409+ilda24@users.noreply.github.com>
2022-11-07 20:14:46 +0100

    save

---
+++ code/IdeaProjects/ilda-demo/src/main/java/net/rumq/ildademo/IldaDemoApplication.java
@@ -0,0 +1,15 @@
+package net.rumq.ildademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IldaDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IldaDemoApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ code/IdeaProjects/ilda-demo/src/test/java/net/rumq/ildademo/IldaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rumq.ildademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IldaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
