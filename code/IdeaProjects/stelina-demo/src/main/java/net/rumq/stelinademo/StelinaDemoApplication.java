fc380e85e08630677c53608bfa1ef7b894402e69
stelina <stelina.pema@gmail.com>
2022-11-07 20:01:09 +0100

    added a new project

---
+++ code/IdeaProjects/stelina-demo/src/main/java/net/rumq/stelinademo/StelinaDemoApplication.java
@@ -0,0 +1,13 @@
+package net.rumq.stelinademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StelinaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StelinaDemoApplication.class, args);
+	}
+
+}
---
+++ code/IdeaProjects/stelina-demo/src/test/java/net/rumq/stelinademo/StelinaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rumq.stelinademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StelinaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
