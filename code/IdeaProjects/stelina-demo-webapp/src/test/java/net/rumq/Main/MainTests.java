8f3b7a8b42c17a987166aa7b419fea181a85cda0
stelina <stelina.pema@gmail.com>
2022-11-08 17:47:42 +0100

    save

---
+++ code/IdeaProjects/stelina-demo-webapp/src/test/java/net/rumq/Main/MainTests.java
@@ -0,0 +1,13 @@
+package net.rumq.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
