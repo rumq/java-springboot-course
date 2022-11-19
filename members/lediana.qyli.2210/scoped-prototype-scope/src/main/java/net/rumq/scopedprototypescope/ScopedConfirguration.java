e2e38178fcf2b46cfacd83ab6203bee871639900
Lediana <ledianaqyli@gmail.com>
2022-11-19 20:11:53 +0100

    Assignment  #1

--- members/lediana.qyli.2210/scoped-prototype-scope/src/main/java/net/rumq/scopedprototypescope/ScopedConfirguration.java
+++ members/lediana.qyli.2210/scoped-prototype-scope/src/main/java/net/rumq/scopedprototypescope/ScopedConfirguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class ScopedConfirguration {
     @Bean
-    @Scope(value="singleton")
+    @Scope(value="singleton") //to run project in prototype scope change value="prototype"
     public ScopedClass scopedclass(){
         return  new ScopedClass();
     }
--- members/lediana.qyli.2210/scoped-prototype-scope/src/main/java/net/rumq/scopedprototypescope/ScopedPrototypeScopeApplication.java
+++ members/lediana.qyli.2210/scoped-prototype-scope/src/main/java/net/rumq/scopedprototypescope/ScopedPrototypeScopeApplication.java
@@ -3,12 +3,12 @@ package net.rumq.scopedprototypescope;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class ScopedPrototypeScopeApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(ScopedPrototypeScopeApplication.class, args);
 
 		AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
 		ctx.register(ScopedConfirguration.class);
@@ -16,6 +16,20 @@ public class ScopedPrototypeScopeApplication {
 
 		ScopedClass s1=ctx.getBean(ScopedClass.class);
 		System.out.println(s1);
+
+		ScopedClass s2=ctx.getBean(ScopedClass.class);
+		System.out.println(s2);
+
+//		Result :
+//		@Scope(value="singleton")
+//		only one instance of the spring bean will
+//		be created for the spring container (default spring).
+//		@Scope(value="prototype")
+//		A new instance will be created every time the
+//		bean is requested from the spring container.
+
+
+
 	}
 
 }
