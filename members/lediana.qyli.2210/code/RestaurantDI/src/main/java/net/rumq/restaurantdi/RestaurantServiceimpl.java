b319a6aa0aa7d89b15bb9ee24e29ed40b15ec465
Lediana <ledianaqyli@gmail.com>
2022-11-24 16:14:25 +0100

    Assignment  #8

---
+++ members/lediana.qyli.2210/code/RestaurantDI/src/main/java/net/rumq/restaurantdi/RestaurantServiceimpl.java
@@ -0,0 +1,19 @@
+package net.rumq.restaurantdi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service("RestaurantService")
+
+public class RestaurantServiceimpl implements RestaurantService{
+
+    @Autowired
+    private RestaurantRepository repository;
+
+    @Override
+    public void service(String menu, String staff) {
+        System.out.println("From RestaurantService: "+menu+" Ready \n"+staff+"Ready!");
+        repository.information(menu, staff);
+    }
+}
