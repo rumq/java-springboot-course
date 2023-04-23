6ec8c26d210f6d7c27a40a41ad3548dd2d1304f9
adadedja <adadedja20@gmail.com>
2023-04-23 20:14:29 +0200

    Restaurant web-app #51

---
+++ members/ada.dedja.2304/RestaurantDomain/RestaurantWebApp/src/main/java/com/example/restaurantwebapp/RestaurantWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantWebAppApplication.class, args);
+    }
+
+}
---
+++ members/ada.dedja.2304/RestaurantDomain/RestaurantWebApp/src/test/java/com/example/restaurantwebapp/RestaurantWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
