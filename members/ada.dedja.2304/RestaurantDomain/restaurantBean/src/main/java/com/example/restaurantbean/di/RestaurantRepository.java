b2a5dea22291d53f419fea1ec989033f9d6b2478
PawarV <pawar@rumq.net>
2023-05-08 07:31:39 +0100

    Cleanup - remove all code of members

--- members/ada.dedja.2304/RestaurantDomain/restaurantBean/src/main/java/com/example/restaurantbean/di/RestaurantRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.restaurantbean.di;
-
-public interface RestaurantRepository {
-    void update(String name, int price);
-}
--- members/ada.dedja.2304/RestaurantDomain/restaurantBean/src/main/java/com/example/restaurantbean/di/RestaurantService.java
+++
@@ -1,6 +0,0 @@
-package com.example.restaurantbean.di;
-
-public interface RestaurantService {
-    void showDish(String name, int price);
-}
-
